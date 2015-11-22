7aec5a22c99e213794ad63235c33c0ef54283585
Chris Rush <crush@nacmtampa.com>
2015-11-22 00:53:48 -0700

    Refactoring Template Params, StringId, Crcs, and various other foundation data types. SWG Module should now compile.

--- src/main/java/com/ocdsoft/bacta/swg/precu/object/template/ServerCreatureObjectTemplate.java
+++ src/main/java/com/ocdsoft/bacta/swg/precu/object/template/ServerCreatureObjectTemplate.java
@@ -1,7 +1,6 @@
 package com.ocdsoft.bacta.swg.precu.object.template;
 
 import com.ocdsoft.bacta.swg.shared.object.template.SharedCreatureObjectTemplate;
-import com.ocdsoft.bacta.swg.shared.object.template.param.IntegerParam;
 
 /**
  * Created by crush on 3/6/14.
