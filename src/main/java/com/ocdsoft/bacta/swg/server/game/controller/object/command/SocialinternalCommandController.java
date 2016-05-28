131aa7ac530f24460c1a04eaf201da42d10d2d1c
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-05-28 01:13:47 -0600

    [Fix] Controller / Message generator paths and templates

--- src/main/java/com/ocdsoft/bacta/swg/server/game/controller/object/command/SocialinternalCommandController.java
+++ src/main/java/com/ocdsoft/bacta/swg/server/game/controller/object/command/SocialinternalCommandController.java
@@ -1,4 +1,4 @@
-package $packageName;
+package com.ocdsoft.bacta.swg.server.game.controller.object.command;
 
 import com.ocdsoft.bacta.soe.connection.SoeUdpConnection;
 import com.ocdsoft.bacta.soe.controller.MessageHandled;
@@ -6,12 +6,12 @@ import com.ocdsoft.bacta.swg.server.game.object.tangible.TangibleObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.ocdsoft.bacta.soe.controller.CommandController;
-import $messageClasspath;
+import com.ocdsoft.bacta.swg.server.game.message.object.command.Socialinternal;
 
-@MessageHandled(handles = ${messageName}.class)
-public class $className implements CommandController<TangibleObject> {
+@MessageHandled(handles = Socialinternal.class)
+public class SocialinternalCommandController implements CommandController<TangibleObject> {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(${className}.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(SocialinternalCommandController.class);
 
 	@Override
 	public void handleCommand(final SoeUdpConnection connection, final TangibleObject invoker, final TangibleObject target, final String params) {
