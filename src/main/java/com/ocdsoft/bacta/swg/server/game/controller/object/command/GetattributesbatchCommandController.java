1fcf176ec90a6a059adb17a1579b039197bd786d
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-05-28 11:50:02 -0600

    [Add] Multiple message handling for GameNetworkMessages

---
+++ src/main/java/com/ocdsoft/bacta/swg/server/game/controller/object/command/GetattributesbatchCommandController.java
@@ -0,0 +1,21 @@
+package com.ocdsoft.bacta.swg.server.game.controller.object.command;
+
+import com.ocdsoft.bacta.soe.connection.SoeUdpConnection;
+import com.ocdsoft.bacta.soe.controller.MessageHandled;
+import com.ocdsoft.bacta.swg.server.game.object.tangible.TangibleObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.ocdsoft.bacta.soe.controller.CommandController;
+import com.ocdsoft.bacta.swg.server.game.message.object.command.Getattributesbatch;
+
+@MessageHandled(handles = Getattributesbatch.class)
+public class GetattributesbatchCommandController implements CommandController<TangibleObject> {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(GetattributesbatchCommandController.class);
+
+	@Override
+	public void handleCommand(final SoeUdpConnection connection, final TangibleObject invoker, final TangibleObject target, final String params) {
+		LOGGER.warn("This command not implemented");
+	}
+	
+}
